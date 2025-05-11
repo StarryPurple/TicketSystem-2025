f834dccb7a5938248f8b2c6f21833fe7cef4fb38
StarryPurple <starrypurple@163.com>
2025-05-11 14:38:49 +0800

    ACM OnlineJudge Test.

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ void BptTest() {
   std::filesystem::create_directory(dir);
   auto name_base = dir / "test";
   int k_param = 4;
-  int buffer_cap = 1024;
+  int buffer_cap = 128;
   int thread_num = 1;
   MulBpt_t mul_bpt(name_base, k_param, buffer_cap, thread_num);
 
